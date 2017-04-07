07674008f5a1d83ed12dc7ac8f2648b8f011b6a6
maximov8 <maximum211155@gmail.com>
2017-04-07 16:49:43 +0300

    Added index.html

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import path from 'path'
 
 import mongoose from 'mongoose'
 import ctl from './controllers/url.controllers'
 
 let app = express()
 app.use(bodyParser.json())
+app.use(express.static('src'))
 
 let uri = 'mongodb://newbee:newbee@ds155080.mlab.com:55080/url_shortener'
 mongoose.connect(process.env.MONGODB_URI || uri, (err) => {
@@ -15,6 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || uri, (err) => {
   console.log("Everything is fine")
 })
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
 app.get('/new/:url*', ctl.run_shorten)
 app.get('/:short', ctl.redirectOrig)
 
